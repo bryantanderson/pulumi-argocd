9b24adffb332d097a2933b53fc1f4a8608e5742a
Bryant <andersonciputra@gmail.com>
2025-05-16 16:38:43 +0800

    chore: add dummy configmap to test argocd syncing

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,23 @@ async function main() {
   });
 
   deployArgoCD(k8sProvider);
+
+  // Dummy ConfigMap to test ArgoCD sync behavior
+  if (process.env.DUMMY_CONFIG_MAP_ENABLED) {
+    new k8s.core.v1.ConfigMap(
+      `dummy-config-map`,
+      {
+        metadata: {
+          name: `dummy-config-map`,
+          namespace: "default",
+        },
+        data: {
+          "dummy-key": "dummy-value",
+        },
+      },
+      { provider: k8sProvider }
+    );
+  }
 }
 
-main()
+main();
