bb3691a63fd0a81e2b5fc597f3e2340a368465e0
Bryant <andersonciputra@gmail.com>
2025-05-16 20:17:33 +0800

    feat: simple gin api to handle post requests

--- app/main.go
+++ app/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+func handlePing(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "pong"})
+}
 
 func main() {
 	fmt.Println("API server is running...")
+	router := gin.Default()
+	router.POST("/ping", handlePing)
+	router.Run("localhost:8080")
 }
